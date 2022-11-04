45fee3aafd8261e2f19a64f75ed82baf6881362c
usha-12 <usha12ahirwar@gmail.com>
2022-11-04 15:24:01 +0530

    LinkedList Program

--- src/usha/collectionframeworkwithlinkedlist/LinkedList.java
+++ src/usha/collectionframeworkwithlinkedlist/LinkedList.java
@@ -1,2 +1,28 @@
-package usha.collectionframeworkwithlinkedlist;public class LinkedList {
+package usha.collectionframeworkwithlinkedlist;
+
+import java.util.LinkedList;
+
+class LL {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+        list.addFirst("usha");
+        list.addFirst("preeti");
+        System.out.println(list);
+        list.addFirst("Avinash");
+        System.out.println(list);
+        System.out.println(list.size());
+        for (int i =0; i<list.size(); i++){// iterate list
+            if (list.get(i) == "usha")//search value in index
+            System.out.print(list.get(i)+"->");
+        }
+        System.out.println("null");
+        /*list.removeFirst();
+        System.out.println(list);
+        list.removeLast();
+        System.out.println(list);
+         */
+        list.remove(0);//when we delete index value in list use remove function
+        System.out.println(list);
+    }
+
 }
--- src/usha/linkedlist/LL.java
+++ src/usha/linkedlist/LL.java
@@ -1,2 +1,101 @@
-package usha.linkedlist;public class LL {
+package usha.linkedlist;
+ class LL {
+     Node head;
+     private int size;
+
+      LL() {
+         this.size = 0;
+     }
+
+     class Node{
+        String data;
+        Node next;
+
+         Node(String data) {
+            this.data = data;
+            this.next = null;
+            size++;
+        }
+    }
+    // add- first, last
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+     public void addLast(String data){
+        Node newNode = new Node(data);
+        if (head == null){
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null){
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+
+     }
+     public void deleteFirst(){
+        if (head == null){
+            System.out.println("This list is empty");
+            return;
+        }
+        size--;
+        head = head.next;
+     }
+     public void deleteLast(){
+         if (head == null){
+             System.out.println("This list is empty");
+             return;
+         }
+         size--;
+         if (head.next == null){
+             head = null;
+             return;
+         }
+         Node secondLast = head;
+         Node lastNode = head.next;// head.next = null -> lastNode = null
+         while (lastNode.next != null){ // null.next
+             lastNode = lastNode.next;
+             secondLast = secondLast.next;
+         }
+         secondLast.next = null;
+     }
+     public void printList() {
+         Node currNode = head;
+
+         while (currNode != null) {
+             System.out.print(currNode.data + " -> ");
+             currNode = currNode.next;
+         }
+
+         System.out.println("null");
+     }
+
+     public int getSize() {
+         return size;
+     }
+
+     public static void main(String[] args) {
+         LL list = new LL();
+         list.addFirst("usha");
+         list.addFirst("aditya");
+         list.printList();
+         list.addLast("preeti");
+         list.printList();
+         list.addFirst("usha");
+         list.printList();
+         list.deleteFirst();
+         list.printList();
+         list.deleteLast();
+         list.printList();
+         list.getSize();
+         System.out.println(list.getSize());
+
+
+
+     }
+
 }
